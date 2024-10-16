8ddf082d2f8442a4cef96fa61eaaa502271df171
Sushruth Nagaraj <sushruthnagaraj2001@gmail.com>
2024-10-16 09:27:13 +0530

    Initial commit

---
+++ src/main/java/io/github/sushnag22/weatherinfo/WeatherInfoApplication.java
@@ -0,0 +1,13 @@
+package io.github.sushnag22.weatherinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherInfoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/sushnag22/weatherinfo/WeatherInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.sushnag22.weatherinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
