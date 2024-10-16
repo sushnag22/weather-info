5ee87bdb2f07d998fe0d6aab617b154fbdd84532
Sushruth Nagaraj <sushruthnagaraj2001@gmail.com>
2024-10-16 21:15:53 +0530

    Fix failing tests by adding dummy values for environment variables

--- src/test/java/io/github/sushnag22/weatherinfo/WeatherInfoApplicationTests.java
+++ src/test/java/io/github/sushnag22/weatherinfo/WeatherInfoApplicationTests.java
@@ -1,10 +1,23 @@
 package io.github.sushnag22.weatherinfo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 
 @SpringBootTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
 class WeatherInfoApplicationTests {
+
+	@DynamicPropertySource
+	static void setProperties(DynamicPropertyRegistry registry) {
+		registry.add("OPENWEATHERMAP_API_KEY", () -> "TEST_VALUE_1");
+		registry.add("OPENWEATHERMAP_URL", () -> "TEST_VALUE_2");
+		registry.add("RAPIDAPI_API_KEY", () -> "TEST_VALUE_3");
+		registry.add("RAPIDAPI_HOST", () -> "TEST_VALUE_4");
+		registry.add("RAPIDAPI_URL", () -> "TEST_VALUE_5");
+	}
 
 	@Test
 	void contextLoads() {
