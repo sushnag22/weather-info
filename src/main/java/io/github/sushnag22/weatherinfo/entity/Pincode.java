6ecd749ff9e9fc5cb6944c9e3445bae34ced41d5
Sushruth Nagaraj <sushruthnagaraj2001@gmail.com>
2024-10-16 09:49:12 +0530

    Add entity class for pincode

---
+++ src/main/java/io/github/sushnag22/weatherinfo/entity/Pincode.java
@@ -0,0 +1,24 @@
+package io.github.sushnag22.weatherinfo.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "pincode")
+@Getter
+@Setter
+public class Pincode {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String pincode;
+
+    private Double latitude;
+
+    private Double longitude;
+
+}
